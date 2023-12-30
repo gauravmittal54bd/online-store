7d0ac38e569aff9c1a537745f1d0a7fba78cd04b
gauravmittal54bd <63183432+gauravmittal54bd@users.noreply.github.com>
2023-12-30 20:25:33 +0530

    Update BasicRating.js

--- src/Component/BasicRating.js
+++ src/Component/BasicRating.js
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import Rating from '@mui/material/Rating';
 
-export default function BasicRating({ value }) {
-
+export default function BasicRating({ value = 0 }) {
   return (
-    
-      <Rating
-        name="simple-controlled"
-        value={value?value:0}
-        // onChange={(event, newValue) => {
-        //   setValue(newValue);
-        // }}
-/>
-  )}
+    <Rating
+      name="simple-controlled"
+      value={value}
+      // onChange={(event, newValue) => {
+      //   setValue(newValue);
+      // }}
+    />
+  );
+}
