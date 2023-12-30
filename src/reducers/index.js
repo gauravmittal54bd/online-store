61829055b209a7412ae9cb63d061d7101745bd24
gauravmittal54bd <63183432+gauravmittal54bd@users.noreply.github.com>
2023-12-30 20:03:58 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,6 +13,7 @@ let initialState = {
   itemToDisplay: "",
   totalCart: 0,
 };
+
 export default function products(state = initialState, actions) {
   switch (actions.type) {
     case Add_products:
@@ -20,10 +21,10 @@ export default function products(state = initialState, actions) {
         ...state,
         products: actions.products,
       };
-      break;
+
     case Add_cart:
       let flag = state.cart.indexOf(actions.cart);
-      if (flag!==-1) {
+      if (flag !== -1) {
         actions.cart.qty += 1;
         return {
           ...state,
@@ -34,14 +35,13 @@ export default function products(state = initialState, actions) {
           cart: [actions.cart, ...state.cart],
         };
       }
-      break;
 
     case Product_view:
       return {
         ...state,
         itemToDisplay: actions.view,
       };
-      break;
+
     case Cart_items:
       let { cart } = state;
       let total = cart.reduce((total, item) => {
@@ -51,7 +51,7 @@ export default function products(state = initialState, actions) {
         ...state,
         totalCart: total,
       };
-      break;
+
     case update_cart:
       let index = state.cart.indexOf(actions.updatedItem);
       let updatedCart = null;
@@ -63,12 +63,14 @@ export default function products(state = initialState, actions) {
         ...state,
         cart: [...updatedCart],
       };
+
     case delete_cart:
       let position = state.cart.indexOf(actions.item);
       state.cart.splice(position, 1);
       return {
         ...state,
-      }
+      };
+
     default:
       return state;
   }
